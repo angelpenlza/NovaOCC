e7552d049fd214b680861dc197af9eac956f410d
angel <angelpmagallon10@gmail.com>
2025-09-24 16:08:38 -0700

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import './navbar.js'
+import NavBar from './navbar.js';
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+  );
+}
---
+++ src/pages/maps.js
@@ -0,0 +1,7 @@
+export default function Maps() {
+  return (
+    <div>
+      <h1>maps</h1>
+    </div>
+  );
+}
